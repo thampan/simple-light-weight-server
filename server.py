0e8ef3b85de198f792da3e6c5c1d1646644c0ee5
Jishnu M Thampan <jishnu.mt@gmail.com>
2019-01-23 20:36:52 +0100

    Create server.py
    version 0

---
+++ server.py
@@ -0,0 +1,18 @@
+#/***************************************************************************
+#============= Copyright by Darmstadt University of Applied Sciences =======
+#***************************************************************************
+# Filename        : server.py
+# Author          : Jishnu M Thampan
+# Description     : Simple script to host your workspace into an ftp server.
+#					Server could be accessible by navigating to locaalhost:80
+#***************************************************************************/
+
+import os, sys
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+webdir = '.'
+port   = 80    
+os.chdir(webdir)                                       
+srvraddr = ("", port)                                  # hostname and portnumber
+srvrobj  = HTTPServer(srvraddr, CGIHTTPRequestHandler)
+srvrobj.serve_forever()                                # run until user closes
